7b134b46baa28e50ec1f84d4eb4214c1ab29255f
HiramHe <776748935@qq.com>
2019-01-16 11:42:56 +0800

    后台系统

---
+++ backgroundSystem/bs_parent_taskOfDeveloper1/src/main/java/cn/edu/hust/service/UserService.java
@@ -0,0 +1,4 @@
+package cn.edu.hust.service;
+
+public class UserService {
+}
---
+++ backgroundSystem/bs_parent_web/src/main/java/cn/edu/hust/controller/UserController.java
@@ -0,0 +1,11 @@
+package cn.edu.hust.controller;
+
+import cn.edu.hust.service.UserService;
+
+public class UserController {
+    UserService userService;
+
+    public void test1(){
+
+    }
+}
