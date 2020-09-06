5b247a7f1e96478013ad81749158db53259cf229
toAlgorithm <1147413415@qq.com>
2020-09-07 00:23:04 +0800

    drop useless file

--- myspringboot/src/main/java/cn/edu/hust/myspringboot/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.hust.myspringboot.repository;
-
-import cn.edu.hust.myspringboot.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
-
